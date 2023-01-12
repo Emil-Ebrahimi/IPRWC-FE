134ff9faeef35cf56d9c43840221b42bc4f858c8
Emil Ebrahimi <s1132776@hsleiden.nl>
2023-01-12 16:36:24 +0100

    FEAT: ReArranged file structure and added promocodes

---
+++ src/app/promocode/add-promo-codes/add-promo-codes.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ToastrService} from "ngx-toastr";
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {AdminService} from "../../admin.service";
+
+@Component({
+  selector: 'app-add-promo-codes',
+  templateUrl: './add-promo-codes.component.html',
+  styleUrls: ['./add-promo-codes.component.scss']
+})
+export class AddPromoCodesComponent implements OnInit {
+  public loading = false;
+  form:FormGroup;
+
+
+
+
+  constructor(private toastr: ToastrService, private fb:FormBuilder, private router: Router, private http: HttpClient, private adminService: AdminService) {
+
+    this.form = this.fb.group({
+      name: ["", Validators.required],
+      korting: ["", Validators.required]
+    });
+  }
+  ngOnInit(): void {
+  }
+
+
+  onSubmit() {
+
+    const val = this.form.value;
+    if (this.form.valid) {
+      this.loading = true
+      this.adminService.createPromoCode(val.name, val.korting).subscribe({
+        next: () => {
+          this.loading = false;
+          this.router.navigateByUrl('/admin/edit-promocodes');
+          this.toastr.success('Succesvol het promoCode aangemaakt!', 'PromoCodeModel Aangemaakt!')
+        },
+        error: error => {
+          this.loading = false;
+          console.error('There was an error!', error);
+        }
+      });
+
+    } else {
+      console.log('There was an error! Form invalid!');
+    }
+  }
+}
